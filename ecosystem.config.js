82320914c63428769e2c786be136bfb1a777a771
Howard <62272939+juyoungIt@users.noreply.github.com>
2026-01-22 16:19:04 +0900

    infra: add CI/CD Workflow and ecosystem.config.js file for pm2 runtime

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dojangkok-client',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
