b6afd7e46f90f310bc592412e8e178fc24ce5866
bae-suho <sugh6019@naver.com>
2026-02-28 17:44:11 +0900

    feat: S3 이미지 도메인 허용 및 프로필 이미지 sizes 설정

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'img1.kakaocdn.net',
       },
+      {
+        protocol: 'https',
+        hostname: 'ktb-team14-dojangkok-bucket.s3.ap-northeast-2.amazonaws.com',
+      },
     ],
   },
 };
